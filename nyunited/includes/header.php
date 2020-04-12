a343693ef5cbe9add42ab9c1f6152a03cdb619b8
HoltHunter <abdulaziz@DESKTOP-CNRLCPA.localdomain>
2020-04-12 15:15:27 -0500

    adding messages and friends features to the program

--- nyunited/includes/header.php
+++ nyunited/includes/header.php
@@ -1,5 +1,8 @@
 <?php
 require 'config/config.php';
+include("includes/classes/User.php");
+include("includes/classes/Post.php");
+include("includes/classes/Message.php");
 
 if(isset($_SESSION['username']))
 {
@@ -20,12 +23,20 @@ else
 	<title>Welcom to NYUnited</title>
 	<!-- JS -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-	<script type="assets/js/bootstrap.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/bootbox.min.js"></script>
+	<script src="assets/js/nyunited.js"></script>
+	<script src="assets/js/jquery.jcrop.js"></script>
+	<script src="assets/js/jcrop_bits.js"></script>
+
 
 	<!-- CSS -->
 	<link href="assets/css/fontawesome-free-5.12.1-web/css/all.css" rel="stylesheet"> <!--load all styles -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/jquery.Jcrop.css" type="text/css" />
+
 </head>
 <body>
 
@@ -52,7 +63,7 @@ else
 			<a href="#">
 				<i class="far fa-bell"></i>
 			</a>
-			<a href="#">
+			<a href="requests.php">
 				<i class="fas fa-users"></i>
 			</a>
 			<a href="includes/handlers/logout.php">
